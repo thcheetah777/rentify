380ddc5b322b7a53610402e904e6b5e6bb286cc4
Leo <thcheetah777@gmail.com>
2023-09-19 09:51:59 +0300

    more setup stuff

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./src/**/*.{svelte,html,js,ts}"],
+  theme: {
+    extend: {
+      colors: {
+        "primary": "#1e88e5",
+      }
+    },
+  },
+  plugins: [],
+}
+
